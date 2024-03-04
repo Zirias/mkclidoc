5383ba3d0ccde528d3c5c71b7e85f86d9435ffb9
Felix Palmen <felix@palmen-it.de>
2024-03-04 08:33:34 +0100

    main: Implement basic commandline arguments

--- src/bin/mkclidoc/main.c
+++ src/bin/mkclidoc/main.c
@@ -1,11 +1,108 @@
 #include "clidoc.h"
 #include "manwriter.h"
+#include "mdocwriter.h"
 
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int (*writer)(FILE *out, const CliDoc *root);
+
+static const struct {
+    const char *name;
+    writer writefunc;
+} writers[] = {
+    { "man", writeMan },
+    { "mdoc", writeMdoc }
+};
+
+static writer currentWriter = writeMan;
+const char *infilename = 0;
+const char *outfilename = 0;
+static FILE *infile = 0;
+static FILE *outfile = 0;
+
+int main(int argc, char **argv)
 {
-    CliDoc *doc = CliDoc_create(stdin);
-    if (!doc) return 1;
-    writeMan(stdout, doc);
+    int flags = 1;
+    char *name = argv[0];
+    const char *arg;
+    if (!name) name = "mkclidoc";
+
+    for (++argv, --argc; argc ; ++argv, --argc)
+    {
+	if (**argv != '-') flags = 0;
+	if (flags) switch((*argv)[1])
+	{
+	    case '-':
+		if ((*argv)[2]) goto usage;
+		flags = 0;
+		break;
+
+	    case 'f':
+		if (!(*argv)[2])
+		{
+		    if (!argc--) goto usage;
+		    arg = *++argv;
+		} else arg = *argv + 2;
+		currentWriter = 0;
+		for (unsigned i = 0; i < sizeof writers / sizeof *writers; ++i)
+		{
+		    if (!strcmp(arg, writers[i].name))
+		    {
+			currentWriter = writers[i].writefunc;
+			break;
+		    }
+		}
+		if (!currentWriter) goto usage;
+		break;
+
+	    case 'o':
+		if (!(*argv)[2])
+		{
+		    if (!argc--) goto usage;
+		    outfilename = *++argv;
+		} else outfilename = *argv + 2;
+		break;
+
+	    default:
+		goto usage;
+	}
+	else
+	{
+	    if (infilename) goto usage;
+	    infilename = *argv;
+	}
+    }
+
+    int rc = EXIT_FAILURE;
+    FILE *in = stdin;
+    CliDoc *doc = 0;
+
+    if (infilename)
+    {
+	if (!(infile = fopen(infilename, "r"))) goto done;
+	in = infile;
+    }
+    FILE *out = stdout;
+    if (outfilename)
+    {
+	if (!(outfile = fopen(outfilename, "w"))) goto done;
+	out = outfile;
+    }
+
+    if (!(doc = CliDoc_create(in))) goto done;
+    if (currentWriter(out, doc) < 0) goto done;
+    rc = EXIT_SUCCESS;
+
+done:
     CliDoc_destroy(doc);
+    if (infile) fclose(infile);
+    if (outfile) fclose(outfile);
+    return rc;
+
+usage:
+    fprintf(stderr, "Usage: %s [-f <man|mdoc>] [-o outfile] [infile]\n", name);
+    return EXIT_FAILURE;
 }
 
